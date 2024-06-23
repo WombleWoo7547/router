04015af94f31970ae9ec262cdec99c7538226f3e
WombleWoo7547 <73031179+WombleWoo7547@users.noreply.github.com>
2024-06-23 10:58:31 +0000

    wompo wopm

--- packages/router/index.js
+++ packages/router/index.js
@@ -1,7 +1,11 @@
 'use strict'
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/vue-router.prod.cjs')
-} else {
-  module.exports = require('./dist/vue-router.cjs')
-}
+(() => {
+  if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./dist/vue-router.prod.cjs')
+  } else {
+    module.exports = require('./dist/vue-router.cjs')
+  }  
+})
+
+console.log('womp womp')
